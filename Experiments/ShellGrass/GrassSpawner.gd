2895ba7b6b8c527a2115c869f3873f346d868049
Cristian Poll <git@cristianpoll.com>
2025-05-08 21:45:15 -0400

    shell fur still broken

--- Experiments/ShellGrass/GrassSpawner.gd
+++ Experiments/ShellGrass/GrassSpawner.gd
@@ -5,13 +5,14 @@ extends MeshInstance3D
 var plane_scene = preload("res://Experiments/ShellGrass/GrassPlane.tscn")
 
 func _ready() -> void:
-    print(grass_plane)
-    for i in range(1, 10):
+    
+    for i in range(0, 40):
+        print(i)
         var dup = plane_scene.instantiate()
+        add_child(dup)
+        dup.position = Vector3(0, 0.003*i, 0)
         var plane = dup.get_node("Plane")
-        plane.position.x += 5;
         plane.set_instance_shader_parameter("stack_order", i)
-        plane.set_instance_shader_parameter("test_color", Vector3(1.0, 0.0, 0.0))
-        # dup.mesh.material
-        # material.set("shader_parameter/stack_order", i);
-        print(plane)
+        ## dup.mesh.material
+        ## material.set("shader_parameter/stack_order", i);
+        #print(plane)
