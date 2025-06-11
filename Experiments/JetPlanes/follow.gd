24bf4617589c2a5e4856b9aecf559a284980c188
Cristian Poll <git@cristianpoll.com>
2025-06-10 20:12:13 -0400

    add crosshair

--- Experiments/JetPlanes/follow.gd
+++ Experiments/JetPlanes/follow.gd
@@ -2,11 +2,10 @@ extends RigidBody3D
 
 var speed = 1
 @export var target: Node3D
-	
+    
 func _physics_process(delta):        
-	if not target:
-		return
-	
-	var direction_to_move = (target.global_position - global_position).normalized()
-	global_position += direction_to_move * speed * delta
-	print(target.global_position, direction_to_move)
+    if not target:
+        return
+    
+    var direction_to_move = (target.global_position - global_position).normalized()
+    global_position += direction_to_move * speed * delta
